da9477f9b131fc1234621b10684112ff9447a2c8
TheOneProgrammerGuy <johnkennethgamora77@gmail.com>
2022-08-18 02:08:53 +0800

    End of Section 12

--- API/DTOs/MemberUpdateDto.cs
+++ API/DTOs/MemberUpdateDto.cs
@@ -11,7 +11,7 @@ namespace API.DTOs
 
         public string LookingFor { get; set; }
 
-        public string Interests { get; set; }
+        public string Interests { get; set; } 
 
         public string City { get; set; }
 
